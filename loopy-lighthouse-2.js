1c265bf6d1b02f8f9896d0d2d853c9d19ab7fe0d
Kevin Mason <krmason2020@gmail.com>
2021-10-11 00:36:37 +0000

    completed loopy light house 2

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -9,16 +9,20 @@
 // loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
 // The loopyLighthouse function should log to the console all the numbers from 15 to 90, except replacing multiples of 2 with "Batty", multiples of 5 with "Beacon", and multiples of 2 and 5 with "BattyBeacon".
 
-for (var i = 100; i <= 200; i++) {
-  if (!(i % 3) && !(i % 4)) {
-    console.log("LoopyLighthouse");
-  } else {
-    if (!(i % 3)) {
-      console.log("Loopy");
-    } else if (!(i % 4)) {
-      console.log("Lighthouse");
+const loopyLighthouse = (range, multiples, words) => {
+  for (var i = range[0]; i <= range[1]; i++) {
+    if (!(i % multiples[0]) && !(i % multiples[1])) {
+      console.log(words[0] + words[1]);
     } else {
-      console.log(i);
+      if (!(i % multiples[0])) {
+        console.log(words[0]);
+      } else if (!(i % multiples[1])) {
+        console.log(words[1]);
+      } else {
+        console.log(i);
+      }
     }
   }
-}
+};
+
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
